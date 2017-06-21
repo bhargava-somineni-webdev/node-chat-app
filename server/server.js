15f0a2bdde5e908c0a3158852284996302bdc1f8
bhargava-somineni <somineni3@gmail.com>
2017-06-21 19:50:34 -0400

    Add connect and disconnect even handlers

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-//console.log(__dirname + '../public');
+//console.log(__dirname + '../public'); gives different path, hence use path.join() as shown below
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+
 app.use(express.static(publicPath));
 
+server.listen(port, () => {
+    console.log(`Server is up on port: ${port}`);
+});
+
+//below code not needed, once public/index.html file 
+//is exposed via middleware using ||const publicPath = path.join(__dirname, '../public');||,
+//we dont need to render it specifically
 // app.get('/', (req, res) => {
 //     res.render('index.html');
 // });
-
-
-
-app.listen(port, () => {
-    console.log(`Server is up on port: ${port}`);
-});
